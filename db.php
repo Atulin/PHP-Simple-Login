d1bf3fa75705b3de547a370e9992247cd762583a
Angius <koumarin@gmail.com>
2020-10-06 20:06:14 +0200

    Updated with MySQL connection instead of SQLite

--- db.php
+++ db.php
@@ -4,10 +4,27 @@ class Db {
 
     /**
      * Database constructor.
+     * @throws PDOException
      */
     public function __construct()
     {
-        $this->conn = new PDO("sqlite:".__DIR__."/database.sql");
+        // Uncomment for SQLite connection
+        // $this->conn = new PDO("sqlite:".__DIR__."/database.sql");
+
+        // Preferably take those from some .env file
+        $servername = "localhost";
+        $username = "username";
+        $password = "password";
+        $dbname = 'MyDB';
+
+        // Connection proper
+        try {
+            $this->conn = new PDO("mysql:host={$servername};dbname={$dbname}", $username, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            throw $e;
+        }
+
     }
 
     /**
